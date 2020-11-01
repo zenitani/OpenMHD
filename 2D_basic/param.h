6af50a63fdb4aa49f5fff762c7b16fcce59c90ce
Seiji Zenitani <zenitani@gmail.com>
2020-11-01 13:31:35 +0900

    Version 20201101

--- 2D_basic/param.h
+++ 2D_basic/param.h
@@ -1,5 +1,5 @@
 !-*- mode: f90 -*-
-  integer, parameter :: version = 20200830
+  integer, parameter :: version = 20201101
   real(8), parameter :: gamma = 5.d0 / 3.d0
 !  real(8), parameter :: gamma = 2.d0
 ! do not edit
