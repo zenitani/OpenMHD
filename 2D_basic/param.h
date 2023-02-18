8d54bc957ce4fc2b1d1d133b51357007015a4cf7
Seiji Zenitani <zenitani@gmail.com>
2023-02-18 22:02:08 +0900

    version 20230218

--- 2D_basic/param.h
+++ 2D_basic/param.h
@@ -1,5 +1,5 @@
 !-*- mode: f90 -*-
-  integer, parameter :: version = 20220501
+  integer, parameter :: version = 20230218
   real(8), parameter :: gamma = 5.d0 / 3.d0
 !  real(8), parameter :: gamma = 2.d0
 ! do not edit
