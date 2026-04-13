f2e82497e59e16b359504b64a9b83ab3365773e9
Seiji Zenitani <zenitani@gmail.com>
2026-04-13 12:23:36 +0200

    version 20260413

--- param.h
+++ param.h
@@ -1,5 +1,5 @@
 !-*- mode: f90 -*-
-  integer, parameter :: version = 20260305
+  integer, parameter :: version = 20260413
   real(8), parameter :: gamma = 5.d0 / 3.d0
 !  real(8), parameter :: gamma = 2.d0
 ! do not edit
